b88c5c4424bb42fe6ac349cab07434e9b16c2b7a
PedroBrandao77 <pedrofreitas250507@gmail.com>
2025-03-14 17:35:32 -0300

    Create entrada1.py

---
+++ entrada1.py
@@ -0,0 +1,14 @@
+# apagando a tela
+import os
+os.system("cls")
+
+"""
+# entrada de dados
+nome = input("Nome:Pedro " )
+print("Nome = ", nome)
+"""
+
+num = float(input("Digite um numero: ")) # o input le somente dado str()
+# num = float(num)
+dobro = num * 2
+print("Dobro = ", dobro)
