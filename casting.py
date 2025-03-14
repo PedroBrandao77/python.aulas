79bc56d7ad4b74a1dca536cc0891c4c92b255353
PedroBrandao77 <pedrofreitas250507@gmail.com>
2025-03-14 17:05:28 -0300

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,12 @@
+import os
+os.system("clear")
+
+# Casting - Mudança de tipo de variável
+valor = 50
+print(valor, type(valor))
+valor = True
+print(valor, type(valor))
+valor = "Agora eu vou para o ceu"
+print(valor, type(valor))
+valor = 34.5
+print(valor, type(valor))
---
+++ prog01.py
@@ -0,0 +1,17 @@
+import os
+os.system("clear") # apaga a tela de execução
+print("Rodando o Python pelo VS Code")
+print("teste")
+
+# para comentarmos uma linha usamos hashtag
+
+# comentário de diversas linhas - Triple Quotes """ ou '''
+"""
+asfalksdjfladks;jf
+dsfdsfadsfas
+asd
+fadsf
+dsafads
+fsdsadfdsfsdffds"
+"""
+print("comentei multiplas linhas")
