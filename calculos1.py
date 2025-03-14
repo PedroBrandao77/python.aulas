3541d0433a75823e67a443bee07c343bddbe9c7e
PedroBrandao77 <pedrofreitas250507@gmail.com>
2025-03-14 17:21:10 -0300

    Update calculos1.py

--- calculos1.py
+++ calculos1.py
@@ -1,1 +1,22 @@
+# apagando a tela
+import os
+os.system("cls")
+# Operadores aritmeticos
+
+valor1 = 10
+valor2 = 3
+resp = valor1 + valor2
+print("Resposta =", resp)
+resp = valor1 - valor2
+print("Resposta = ", resp)
+resp= valor1 * valor2
+print("Resposta = ", resp)
+resp = valor1 / valor2
+print("Resposta = ", resp)
+resp = valor1 // valor2
+print("Resposta = ", resp)
+resp = valor1 % valor2
+print("Resposta = ", resp)
+resp = valor1 ** valor2
+print("Resposta = ", resp)
 
