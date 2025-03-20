3c068a2f56be8a0d4af161c53c1eb41c5bffd596
PedroBrandao77 <pedrofreitas250507@gmail.com>
2025-03-20 11:41:01 -0300

    Update DecisaoComposta.py

--- DecisaoComposta.py
+++ DecisaoComposta.py
@@ -1,1 +1,58 @@
+import os
+os.system("cls")
 
+'''idade = int(input("Digite a sua idade:"))
+if idade >=18
+    print("Maior de idade")
+else:
+    print("Menor de idade")'''
+     
+     
+     
+     
+""" #exercicio 1    
+numero= int(input("Digite um numero:"))
+if numero >=0:
+    print("numero positivo")
+else:
+    print("numero negativo")"""
+    
+    
+  #exercicio 3
+  
+"""numero= int(input("Digite um numero:"))
+if numero%2 ==0:
+    print("numero par")
+else: 
+    print("numero impar")"""
+        
+        
+''' #exercicio 4
+n1=int(input("Digite um numero "))
+n2=int(input("Digite outro numero "))
+if n1 > n2:
+    print("Maior numero", n1)
+else: 
+    print("Maior numero", n2)'''
+    
+    
+'''#exercicio 5 
+
+n1=float(input("Digite uma nota: "))
+n2=float(input("Digite outra nota: "))
+media = (n1 + n2) /2
+if media >5:
+    print("aprovado = media", media)
+else:
+    print("reprovado", media)'''
+
+
+'''#exercicio 6
+
+compra = int(input("Digite o valor da compra: "))
+d1=(compra)-(0.10*compra)
+d2=(compra)-(0.05*compra)
+if compra>1000:
+    print(f"desconto R$ {d1}")
+else:
+    print(f"valor R$ {d2}")'''
